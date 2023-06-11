56b67d550e45b55a4da3c51a1d698cef97717343
Joao Vitor <jvfs12th@gmail.com>
2023-06-11 09:39:45 -0300

    A very Big sum

---
+++ HackerRank/AVeryBigSum.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class AVeryBigSum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long sum = 0;
+
+        int n = sc.nextInt();
+        Integer ar[] = new Integer[n];
+        sc.nextLine();
+
+         String line = sc.nextLine();
+        String[] valueOfLine = line.split(" ");
+
+        for (int i = 0; i < ar.length; i++) {
+            ar[i] = Integer.parseInt(valueOfLine[i]);
+        }
+
+        for (int i = 0; i <= (ar.length - 1); i++) {
+            sum = sum + ar[i];
+        }
+
+        System.out.println(sum);
+
+        sc.close();
+    }
+}
